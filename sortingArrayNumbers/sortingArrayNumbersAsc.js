4bcf85b6b2d6ca639f6254fb7a700618c661f2d3
Idequel Bernabel Álvarez <63077664+ibernabel@users.noreply.github.com>
2022-07-18 18:06:24 -0400

    Create sortingArrayNumbersAsc.js

---
+++ sortingArrayNumbers/sortingArrayNumbersAsc.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let numbers = [8, 6, 3, 9, 2, 4, 7, 5, 1 ];
+// let numbers = [ 10, 3, 7, 5, 8]; // 3, 5, 7, 8, 10
+console.group("Original");
+    console.log( numbers);
+console.groupEnd("Original");
+
+let arraySorted = sortArrayNumbersAscending( numbers );
+console.group("Sorted");
+    console.log(arraySorted);
+console.groupEnd("Sorted");
+
+function sortArrayNumbersAscending( array ) {
+    let sortedAscendingArray;
+    let i = array.length - 1;
+
+    while (i) {
+        sortedAscendingArray = putMinorValueInFirst(array);
+        i--;
+    }
+
+    return sortedAscendingArray;
+}
+
+function putMinorValueInFirst(array ) {
+    let tempArray = array;
+    let i = array.length - 1;
+    let j = i - 1;
+    let a;
+    let b;
+    
+    while (i) {
+
+        j = i - 1;
+        a = array[i];
+        b = array[j];
+
+        if ( iSMinorNumber(a, b ) ) {
+            tempArray[i] = b;
+            tempArray[j] = a;
+        }
+        i--;
+    }
+
+    return tempArray;
+}
+
+function iSMinorNumber( a , b  ) { return a < b }
